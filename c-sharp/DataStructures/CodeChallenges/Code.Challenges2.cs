62e39ae61db8de261294e9095ce0ce042f4e1101
Odai Al-Obeidat <odai.al.obeidat950@gmail.com>
2023-06-16 20:06:18 +0300

    lap1

---
+++ c-sharp/DataStructures/CodeChallenges/Code.Challenges2.cs
@@ -0,0 +1,67 @@
+
+using System;
+using System.Reflection;
+
+
+namespace Challenge1
+{
+  public class Whiteboard
+  {
+
+
+    static void Main(string[] args)
+    {
+      int[] arr = new int[] { 2, 9, 16, 26, 55, 70, 80 };
+      int integerX = 33;
+
+      Console.WriteLine(string.Join(", ", ShiftArr(arr, integerX)));
+
+
+    }
+
+    public static int[] ShiftArr(int[] arr, Int32 integerX)
+    {
+      int[] NewArray = new int[arr.Length + 1];
+
+      if (arr.Length <= 0 && integerX == null)
+      {
+        return null;
+      }
+      else if (arr.Length <= 0 && integerX != null)
+      {
+        NewArray[0] = integerX;
+        return NewArray;
+      }
+      else
+      {
+        for (int i = 0; i < arr.Length; i++)
+        {
+          if (integerX <= arr[i])
+          {
+            NewArray[i] = integerX;
+            for (int j = i; j < arr.Length; j++)
+            {
+              NewArray[j + 1] = arr[j];
+            }
+            return NewArray;
+            break;
+          }
+          else
+          {
+            NewArray[i] = arr[i];
+          }
+
+        }
+        return NewArray;
+      }
+    }
+
+  }
+}
+
+
+
+
+
+
+
