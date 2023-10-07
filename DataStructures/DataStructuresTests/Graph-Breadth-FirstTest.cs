922cd834c6f30ce69113ac5ebd626793398b16ca
Odai Al-Obeidat <odai.al.obeidat950@gmail.com>
2023-10-07 20:33:23 +0300

    CC36,37,38

---
+++ DataStructures/DataStructuresTests/Graph-Breadth-FirstTest.cs
@@ -0,0 +1,165 @@
+using Graphs;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GraphTest
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void CanAddNodeToGraph()
+    {
+      Graph graph = new Graph();
+
+      Vertex node = (Vertex)graph.AddNode("A");
+
+      Assert.Contains(node, graph.Vertices);
+    }
+
+    [Fact]
+    public void EdgeCanBeAddedToGraph()
+    {
+      Graph graph = new Graph();
+      Vertex A = (Vertex)graph.AddNode("A");
+      Vertex B = (Vertex)graph.AddNode("B");
+      Vertex C = (Vertex)graph.AddNode("C");
+
+      graph.AddEdge(A, B, 15);
+
+      Assert.NotEmpty(A.Edge);
+    }
+
+    [Fact]
+    public void CanGetGraphNodeList()
+    {
+      Graph graph = new Graph();
+      graph.AddNode("A");
+      graph.AddNode("B");
+      graph.AddNode("C");
+      graph.AddNode("D");
+      graph.AddNode("E");
+      graph.AddNode("F");
+      graph.AddNode("G");
+
+      var list = graph.GetNodes();
+
+      Assert.IsType<List<Vertex>>(list);
+    }
+
+    [Fact]
+    public void CanGetNeighborsFromNode()
+    {
+      Graph graph = new Graph();
+      Vertex A = (Vertex)graph.AddNode("A");
+      Vertex B = (Vertex)graph.AddNode("B");
+      Vertex C = (Vertex)graph.AddNode("C");
+      graph.AddEdge(A, B, 45);
+      graph.AddEdge(A, C, 9);
+
+      var result = graph.GetNeighbors(A);
+
+      Assert.Equal(2, result.Count);
+    }
+
+    [Fact]
+    public void NieghborNodesHaveWeight()
+    {
+      Graph graph = new Graph();
+      Vertex A = (Vertex)graph.AddNode("A");
+      Vertex B = (Vertex)graph.AddNode("B");
+      graph.AddEdge(A, B, 9999);
+
+      Edge[] result = graph.GetNeighbors(A).ToArray();
+
+      Assert.Equal(9999, result[0].Weight);
+    }
+
+    [Fact]
+    public void CanGetSizeOfGraph()
+    {
+      Graph graph = new Graph();
+      graph.AddNode("A");
+      graph.AddNode("B");
+      graph.AddNode("C");
+      graph.AddNode("D");
+      graph.AddNode("E");
+      graph.AddNode("F");
+      graph.AddNode("G");
+
+      int count = graph.Size();
+
+      Assert.Equal(7, count);
+    }
+
+    [Fact]
+    public void GraphCanHaveOneNodeAndEdge()
+    {
+      Graph graph = new Graph();
+      Vertex A = (Vertex)graph.AddNode("A");
+      graph.AddEdge(A, A, 7);
+
+      List<Vertex> result = graph.GetNodes();
+
+      Assert.Equal(A, result.ToArray()[0]);
+    }
+
+    [Fact]
+    public void EmptyGraphReturnsNull()
+    {
+      Graph graph = new Graph();
+
+      var result = graph.GetNodes();
+
+      Assert.Null(result);
+    }
+
+    [Fact]
+    public void CanReturnRouteAtoG()
+    {
+      Graph graph = new Graph();
+      graph.AddNode("A");
+      graph.AddNode("B");
+      graph.AddNode("C");
+      graph.AddNode("D");
+      graph.AddNode("E");
+
+      Vertex A = new Vertex("A");
+      Vertex B = new Vertex("B");
+      Vertex C = new Vertex("C");
+      Vertex D = new Vertex("D");
+      Vertex E = new Vertex("E");
+
+      graph.AddEdge(A, B, 15);
+      graph.AddEdge(A, C, 7);
+      graph.AddEdge(A, D, 6);
+      graph.AddEdge(B, E, 10);
+      graph.AddEdge(C, E, 80);
+      graph.AddEdge(D, E, 74);
+
+      List<Vertex> list = graph.BreadthFirst(A);
+      object[] result = new object[5];
+      for (int i = 0; i < result.Length; i++)
+      {
+        result[i] = list[i].Value;
+      }
+      object[] expected = { "A", "B", "C", "D", "E" };
+
+      Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CanReturnSelfPointingEdge()
+    {
+      Graph graph = new Graph();
+      graph.AddNode("A");
+      Vertex A = new Vertex("A");
+      graph.AddEdge(A, A, 1);
+
+      List<Vertex> result = graph.BreadthFirst(A);
+
+      Assert.Equal("A", result[0].Edge[0].Neighbor.Value);
+    }
+
+  }
+}
